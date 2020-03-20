5ff299cf382018c8c648bc61d2ed72c8f0e8f9fa
Benjamin Thom <jaminthom@gmail.com>
2020-03-20 16:21:25 -0700

    Update Models/Client.cs to include auto-implemented properties for Phone_Number and Email, update Views/Clients/ views to include Date_Hired and Email properties, update cshtml headers to be the same accros all views

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,8 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
+    public string Phone_Number { get; set; }
+    public string Email { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
